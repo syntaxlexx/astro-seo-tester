59c7fcdc1de49d676bafb4d12722161cfe976807
SyntaxLexx <spencermainaa@gmail.com>
2024-09-16 22:54:37 +0300

    feat: big Bang!

---
+++ src/components/navbar.tsx
@@ -0,0 +1,204 @@
+import { Menu } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+
+const Navbar = () => {
+  return (
+    <section className="py-4">
+      <div className="container">
+        <nav className="hidden justify-between lg:flex">
+          <div className="flex items-center gap-6">
+            <div className="flex items-center gap-2">
+              <img
+                src="https://www.shadcnblocks.com/images/block/block-1.svg"
+                className="w-8"
+                alt="logo"
+              />
+              <span className="text-xl font-bold">Shadcn Blocks</span>
+            </div>
+            <div className="flex items-center">
+              <a
+                className={cn(
+                  "text-muted-foreground",
+                  navigationMenuTriggerStyle,
+                  buttonVariants({
+                    variant: "ghost",
+                  })
+                )}
+                href="#">
+                Home
+              </a>
+
+              <a
+                className={cn(
+                  "text-muted-foreground",
+                  navigationMenuTriggerStyle,
+                  buttonVariants({
+                    variant: "ghost",
+                  })
+                )}
+                href="#">
+                Pricing
+              </a>
+              <a
+                className={cn(
+                  "text-muted-foreground",
+                  navigationMenuTriggerStyle,
+                  buttonVariants({
+                    variant: "ghost",
+                  })
+                )}
+                href="#">
+                Blog
+              </a>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Button variant={"outline"}>Log in</Button>
+            <Button>Sign up</Button>
+          </div>
+        </nav>
+        <div className="block lg:hidden">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <img
+                src="https://www.shadcnblocks.com/images/block/block-1.svg"
+                className="w-8"
+                alt="logo"
+              />
+              <span className="text-xl font-bold">Shadcn Blocks</span>
+            </div>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant={"outline"} size={"icon"}>
+                  <Menu className="size-4" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="overflow-y-auto">
+                <SheetHeader>
+                  <SheetTitle>
+                    <div className="flex items-center gap-2">
+                      <img
+                        src="https://www.shadcnblocks.com/images/block/block-1.svg"
+                        className="w-8"
+                        alt="logo"
+                      />
+                      <span className="text-xl font-bold">Shadcn Blocks</span>
+                    </div>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="my-8 flex flex-col gap-4">
+                  <a href="#" className="font-semibold">
+                    Home
+                  </a>
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="products" className="border-b-0">
+                      <AccordionTrigger className="mb-4 py-0 font-semibold hover:no-underline">
+                        Products
+                      </AccordionTrigger>
+                    </AccordionItem>
+                    <AccordionItem value="resources" className="border-b-0">
+                      <AccordionTrigger className="py-0 font-semibold hover:no-underline">
+                        Resources
+                      </AccordionTrigger>
+                    </AccordionItem>
+                  </Accordion>
+                  <a href="#" className="font-semibold">
+                    Pricing
+                  </a>
+                  <a href="#" className="font-semibold">
+                    Blog
+                  </a>
+                </div>
+                <div className="border-t pt-4">
+                  <div className="grid grid-cols-2 justify-start">
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Press
+                    </a>
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Contact
+                    </a>
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Imprint
+                    </a>
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Sitemap
+                    </a>
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Legal
+                    </a>
+                    <a
+                      className={cn(
+                        buttonVariants({
+                          variant: "ghost",
+                        }),
+                        "justify-start text-muted-foreground"
+                      )}
+                      href="#">
+                      Cookie Settings
+                    </a>
+                  </div>
+                  <div className="mt-2 flex flex-col gap-3">
+                    <Button variant={"outline"}>Log in</Button>
+                    <Button>Sign up</Button>
+                  </div>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
